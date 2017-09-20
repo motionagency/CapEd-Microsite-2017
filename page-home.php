067e6d2d6fbe0627c05d7aab0b2589d9561fe38a
Frost <cpaccione77@gmail.com>
2017-09-20 17:57:15 -0500

    Home page
    I started the process…

---
+++ page-home.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+
+Template Name: CapEd Home Template
+
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<section class="caped-hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<h2>Re-Imagining</h2>
+				<h2>Online Learning</h2>
+				<p>Highly engaging programs guided by your vision</p>
+				<a class="button" href="#">learn more</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="one">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h3>We don't deliver courses off the shelf. Instead, we'll partner with you on a journey that belong <span>only to you.</span></h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="two">
+	<div class="container">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6">
+			<h3>lorem ipsum dolor.</h3>
+			<p>How We Are Different</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<button>see how we do it</button>
+		</div>
+	</div>
+</section>
+
+<section class="three">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6"></div>
+			<div class="col-sm-6">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<?php get_footer(); ?>
